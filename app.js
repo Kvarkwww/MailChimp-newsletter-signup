0169492eccf7732ad4e9bed64ea9f4b55fe88f54
kvark <info@kvark.si>
2020-07-31 19:49:56 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require("express");
+
+const https = require("https");
+
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html");
+})
+    
+app.post("/", function(req, res){
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName,
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us3.api.mailchimp.com/3.0/lists/989ced3dc2"
+
+    const options = {
+         method: "POST",
+         auth: "kvark:efd13ede5088fa8ad0aa51f3ff5088a2-us3"
+    }
+
+    const request = https.request(url, options, function(response) {
+        if (response.statusCode === 200) {
+            res.sendFile (__dirname + "/success.html");
+        } else {
+            res.sendFile (__dirname + "/failure.html");
+        }
+
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+});
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server running, better catch it");
+}) 
+
+
+
+// MC ApiKey
+// listid 989ced3dc2
+// efd13ede5088fa8ad0aa51f3ff5088a2-us3
